491d89ec8255fb7eaac6b25043d6db6da9fd3e1d
luwanise <praiseoladejo2003@gmail.com>
2026-02-28 18:45:23 +0100

    Fix secure store warning

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,24 +1,18 @@
+import AsyncStorage from "@react-native-async-storage/async-storage";
 import { createClient } from "@supabase/supabase-js";
-import * as SecureStore from "expo-secure-store";
 
 const supabaseUrl = process.env.EXPO_PUBLIC_SUPABASE_PROJECT_URL as string;
 const supabaseKey = process.env.EXPO_PUBLIC_SUPABASE_PUBLISHABLE_KEY as string;
 
-const ExpoSecureStoreAdapter = {
-    getItem: (key: string) => SecureStore.getItemAsync(key),
-    setItem: (key: string, value: any) => SecureStore.setItemAsync(key, value),
-    removeItem: (key: string) => SecureStore.deleteItemAsync(key)
-}
-
 export const supabase = createClient(
-    supabaseUrl,
-    supabaseKey,
-    {
-        auth: {
-            storage: ExpoSecureStoreAdapter,
-            autoRefreshToken: true,
-            persistSession: true,
-            detectSessionInUrl: false
-        }
+  supabaseUrl,
+  supabaseKey,
+  {
+    auth: {
+      storage: AsyncStorage,
+      autoRefreshToken: true,
+      persistSession: true,
+      detectSessionInUrl: false
     }
-)
+  }
+);
